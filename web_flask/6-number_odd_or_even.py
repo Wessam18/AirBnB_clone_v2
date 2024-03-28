6ad524ec43d0b62d6d386044685f483fc6adcacd
wessam18 <wessamabdelnabi903@gmail.com>
2024-03-28 04:27:55 +0200

    5

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """Flask web Application"""
 
@@ -38,6 +39,11 @@ def number(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == "__main__":

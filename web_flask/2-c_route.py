adcb2548bfb2e5838c0007448694f50fba7c59cb
wessam18 <wessamabdelnabi903@gmail.com>
2024-03-29 06:08:11 +0200

    2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-"""Flask web Application"""
+""" Start a web application """
 
 from flask import Flask
 
 
-app = Flask(__name__)
+web_app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello():
+@web_app.route("/", strict_slashes=False)
+def Home():
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
+@web_app.route("/hbnb", strict_slashes=False)
+def prompt():
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    new_text = text.replace('_', ' ')
-    return "C {}".format(new_text)
+@web_app.route("/c/<text>", strict_slashes=False)
+def language(text):
+    new = text.replace('_', ' ')
+    return f"C {new}"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    web_app.run(host="0.0.0.0", port=5000)

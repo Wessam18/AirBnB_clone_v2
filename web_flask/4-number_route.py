880f8575a63e0c843c469642fa36b46eaf4c34e8
wessam18 <wessamabdelnabi903@gmail.com>
2024-04-03 06:38:09 +0200

    -

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -30,13 +30,9 @@ def python(text="is cool"):
     return f"Python {new}"
 
 
-@web_app.route("/number/<n>", strict_slashes=False)
+@web_app.route("/number/<int:n>", strict_slashes=False)
 def integer(n):
-    try:
-        n = int(n)
-        return f"{n} is a number"
-    except ValueError:
-        pass
+    return f"{n} is a number"
 
 
 if __name__ == "__main__":

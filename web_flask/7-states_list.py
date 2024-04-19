431f78c524cadeda7a0fdf4c75d2ed5077d83004
wessam18 <wessamabdelnabi903@gmail.com>
2024-04-19 04:54:44 +0200

    .

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,8 +4,10 @@ starts a Flask web application
 """
 
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models import *
+
+
 app = Flask(__name__)
 
 
@@ -17,9 +19,9 @@ def states_list():
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
+def teardown():
     """closes the storage on teardown"""
     storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000', debug=True)
